cbb97b2663fc171c6062e95efa8634b9e06ec843
Uendici <apesimone06@gmail.com>
2024-09-30 11:02:41 +0000

    inizio

---
+++ struct_c.c
@@ -0,0 +1,27 @@
+#define DIM_COGN 50
+#define NUM_VOTI 5
+#define NUM_STUD 5
+
+void caricaTab(Studente t[]);
+
+// Struttura per la data
+typedef struct Data {
+  int giorno;
+  int mese;
+  int anno;
+} Data;
+// Struttura per lo studente
+typedef struct Studente {
+  char cognome[DIM_COGN];
+  Data data_nascita;
+  int voti[NUM_VOTI];
+} Studente;
+
+int main(){
+    void caricaTab(Studente t);
+}
+
+void caricaTab(Studente t[]){
+    
+
+}
