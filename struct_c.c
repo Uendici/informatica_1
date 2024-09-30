87142c1ab8d52f744450e20dc314f2793f306853
Uendici <apesimone06@gmail.com>
2024-09-30 15:42:10 +0000

    risoluzioni problemi vari ricerCog

--- struct_c.c
+++ struct_c.c
@@ -6,10 +6,6 @@
 #define NUM_VOTI 5
 #define NUM_STUD 5
 
-void caricaTab(Studente t[]);//Carica la tabella chiedendo dati all'utente o generandoli casualmente.
-void stampaTab(Studente t[]);//Stampa la tabella allineando correttamente i dati.
-int ricercCog(Studente t[], char cog[]);//Cerca uno studente per cognome, restituendo l'indice (o -1 se non trovato).
-
 // Struttura per la data
 typedef struct {
   int giorno;
@@ -22,6 +18,10 @@ typedef struct {
   Data data_nascita;
   int voti[NUM_VOTI];
 } Studente;
+
+void caricaTab(Studente t[]);//Carica la tabella chiedendo dati all'utente o generandoli casualmente.
+void stampaTab(Studente t[]);//Stampa la tabella allineando correttamente i dati.
+int ricercCog(Studente t[], char cog[]);//Cerca uno studente per cognome, restituendo l'indice (o -1 se non trovato).
 
 int main(){
   Studente t[NUM_STUD];
@@ -36,35 +36,39 @@ int main(){
     }
 }
   caricaTab(t);
+
   stampaTab(t);
+
   stato_cognome = ricercCog(t,cog);
   if(stato_cognome > -1){
     printf("il cognome si trova nella %d posizione.\n",stato_cognome);
   }
-  
+
 }
 
 void caricaTab(Studente t[]){
-  for(int i = 0; i < NUM_STUD; i++){
-    scanf("%s",&t[i].cognome);
-  }
-  for(int i = 0; i < NUM_STUD; i++){
-    scanf("%d",t[i].data_nascita);    
-  }
-
+  for (int i = 0; i < NUM_STUD; i++) {
+        printf("Inserisci il cognome dello studente %d: ", i + 1);
+        scanf("%s", t[i].cognome); // Legge il cognome
+        
+        printf("Inserisci la data di nascita (gg mm aaaa): ");
+        scanf("%d %d %d", &t[i].data_nascita.giorno, &t[i].data_nascita.mese, &t[i].data_nascita.anno); // Legge la data di nascita
+    }
+}
 void stampaTab(Studente t[]){
   for(int i = 0; i < NUM_STUD; i++){
       printf("%s\t",t[i].cognome);
-      printf("%d\t",t[i].data);
+      printf("%02d/%02d/%04d\t",t[i].data_nascita.giorno,t[i].data_nascita.mese,t[i].data_nascita.anno);
       for(int j = 0; j < NUM_VOTI; j++){
         printf("%d",t[i].voti);
       }
+      printf("\n");
     }
 }
 
 int ricercCog(Studente t[], char cog[]){
   for(int i = 0; i < NUM_STUD; i++){
-    if(t[i].cognome == cog[DIM_COGN]){
+    if (strcmp(t[i].cognome, cog) == 0) {
       return i;
     }else{
       return -1;
