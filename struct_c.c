c7b496ab315c0d0ed3968e98f7e750a88e6b5720
Uendici <apesimone06@gmail.com>
2024-09-30 15:09:01 +0000

    aggiunta StampaTab

--- struct_c.c
+++ struct_c.c
@@ -2,7 +2,8 @@
 #define NUM_VOTI 5
 #define NUM_STUD 5
 
-void caricaTab(Studente t[]);
+void caricaTab(Studente t[]);//Carica la tabella chiedendo dati all'utente o generandoli casualmente.
+void stampaTab(Studente t[]);//Stampa la tabella allineando correttamente i dati.
 
 // Struttura per la data
 typedef struct {
@@ -18,10 +19,20 @@ typedef struct {
 } Studente;
 
 int main(){
-    void caricaTab(Studente t);
+  Studente t[NUM_STUD];
+  caricaTab(t);
+  stampaTab(t);
 }
 
 void caricaTab(Studente t[]){
-    
+  for(int i = 0; i < NUM_STUD; i++){
+    scanf("%s",&t[i].cognome);
+  }
 
+}
+
+void stampaTab(Studente t[]){
+  for(int i = 0; i < NUM_STUD; i++){
+      printf("%d\n",t[i].cognome);
+    }
 }
