dc9b3f66e298fb84be924341f67c3ef65c80370e
Uendici <apesimone06@gmail.com>
2024-09-30 15:32:36 +0000

    aggiunto ricercCog

--- struct_c.c
+++ struct_c.c
@@ -1,9 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 #define DIM_COGN 50
 #define NUM_VOTI 5
 #define NUM_STUD 5
 
 void caricaTab(Studente t[]);//Carica la tabella chiedendo dati all'utente o generandoli casualmente.
 void stampaTab(Studente t[]);//Stampa la tabella allineando correttamente i dati.
+int ricercCog(Studente t[], char cog[]);//Cerca uno studente per cognome, restituendo l'indice (o -1 se non trovato).
 
 // Struttura per la data
 typedef struct {
@@ -20,19 +25,49 @@ typedef struct {
 
 int main(){
   Studente t[NUM_STUD];
+  int stato_cognome;
+  char cog[DIM_COGN];
+
+  srand(time(NULL));
+
+  for (int i = 0; i < NUM_STUD; i++) {
+    for (int j = 0; j < NUM_VOTI; j++) {
+        t[i].voti[j] = rand() % 9 + 1; 
+    }
+}
   caricaTab(t);
   stampaTab(t);
+  stato_cognome = ricercCog(t,cog);
+  if(stato_cognome > -1){
+    printf("il cognome si trova nella %d posizione.\n",stato_cognome);
+  }
+  
 }
 
 void caricaTab(Studente t[]){
   for(int i = 0; i < NUM_STUD; i++){
     scanf("%s",&t[i].cognome);
   }
-
-}
+  for(int i = 0; i < NUM_STUD; i++){
+    scanf("%d",t[i].data_nascita);    
+  }
 
 void stampaTab(Studente t[]){
   for(int i = 0; i < NUM_STUD; i++){
-      printf("%d\n",t[i].cognome);
+      printf("%s\t",t[i].cognome);
+      printf("%d\t",t[i].data);
+      for(int j = 0; j < NUM_VOTI; j++){
+        printf("%d",t[i].voti);
+      }
     }
+}
+
+int ricercCog(Studente t[], char cog[]){
+  for(int i = 0; i < NUM_STUD; i++){
+    if(t[i].cognome == cog[DIM_COGN]){
+      return i;
+    }else{
+      return -1;
+    }
+  }
 }
