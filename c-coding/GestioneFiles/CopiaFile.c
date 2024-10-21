8405608a62251b538ec3d7319d3f2b6ca0cb7113
Uendici <apesimone06@gmail.com>
2024-10-21 10:53:20 +0000

    aggiunta filemaiu e copia

---
+++ c-coding/GestioneFiles/CopiaFile.c
@@ -0,0 +1,29 @@
+//Scrivi  un  programma  che  esegue  la  copia  di  un  file  di  testo.  
+//Il  programma  legge  carattere per carattere il contenuto del file in.txt e lo ricopia nel file out.txt. 
+#include <stdio.h>
+
+int main() {
+    char buffer[100];
+    FILE *file = fopen("FileDiProva.txt", "r");
+    FILE *file2 = fopen("copiato.txt", "w");
+
+    if (file == NULL) {
+        perror("File di origine non trovato");
+        return 1;
+    }
+    if (file2 == NULL) {
+        perror("File di destinazione non trovato");
+        fclose(file);
+        return 1;
+    }
+
+    while (fgets(buffer, 100, file) != NULL) {
+        fputs(buffer, file2);
+    }
+
+    fclose(file);
+    fclose(file2);
+
+    printf("Copia completata con successo!\n");
+    return 0;
+}
--- c-coding/GestioneFiles/FileMaiu.c
+++ c-coding/GestioneFiles/FileMaiu.c
@@ -1,8 +1,11 @@
-//trasforma tutte le lettere minuscole del file in lettere maiuscole 
+
+//Scrivi  un  programma  che  legge  il  file  nomi.txt e  scrive  un  file  NOMI2.TXT,  dove  tutti  i  nomi  sono stati trasformati in caratteri maiuscoli. 
 #include <stdio.h>
 int main(){
     char buffer[100];
-    FILE *file = fopen("FileDiProva.txt", "r");
+    char buffer2[100];
+    FILE *file = fopen("nomi.txt", "r");
+    FILE *file2 = fopen("nomi2.txt", "w");
 
     if(file == NULL){
         perror("file non presente\n");
@@ -11,12 +14,15 @@ int main(){
         while(fgets(buffer, 100, file)!=NULL){
             for (int i = 0; buffer[i] != '\0'; i++) {
                 if (buffer[i] >= 'a' && buffer[i] <= 'z') {
-                    buffer[i] = buffer[i] - 32; // Trasforma il carattere in maiuscolo
+                    buffer[i] = buffer[i] - 32;
                 }
             }
+            fputs(buffer,file2);
         }
+        
         printf("%s\n", buffer);
     }
     fclose(file);
+    fclose(file2);
     return 0;
 }
