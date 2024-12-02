6a4fb44fd92cd21a23bfed8052d239e7eb8d040a
Uendici <apesimone06@gmail.com>
2024-12-02 11:58:16 +0000

    aggiunta frazione in c++

---
+++ cpp/frazione.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std; 
+class Frazione {
+private: 
+int numeratore;
+int denominatore;
+
+public:
+Frazione (){}; 
+void setNumeratore(int n) {
+numeratore = n; }
+void setDenominatore(int n) {
+denominatore = n; }
+int getNumeratore() { return numeratore; }
+int getDenominatore() { return denominatore; }
+void setNumeratoreDenom(int n, int d)
+{ numeratore = n;
+denominatore =d; }
+void stampa() { 
+cout << numeratore << "/" << denominatore;
+int n=getNumeratore(); 
+int d=getDenominatore();
+cout << n<< "/" << d;
+}
+};
+int main() { 
+Frazione f1, f2;
+f1.setNumeratore(12);
+f1.setDenominatore(3);
+f1.stampa();
+system("pause");
+}
