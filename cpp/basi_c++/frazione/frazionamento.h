f44aa9cc1f3e5e5b2c4304480605c36deb98f359
Uendici <apesimone06@gmail.com>
2025-01-24 08:08:46 +0000

    frazionamento aggiunto(da finire)

---
+++ cpp/basi_c++/frazione/frazionamento.h
@@ -0,0 +1,16 @@
+
+class frazione{
+
+    private:
+            int numeratore;
+            int denominatore;
+    public:
+            frazione(int numeratore = 2, int denominatore = 1){
+                this -> numeratore = numeratore;
+                this -> denominatore = denominatore;
+            }
+            int setnumeratore;
+            int setdenominatore;
+            
+
+};
