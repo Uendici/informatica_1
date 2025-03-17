866a63faab17b6079606624542f00393d93013de
Uendici <apesimone06@gmail.com>
2025-03-17 11:57:27 +0000

    liste_concatenateprimoesercizio (da finire)

--- cpp/liste_concatenate/liste_concatenatePrimoEsercizio.cpp
+++ cpp/liste_concatenate/liste_concatenatePrimoEsercizio.cpp
@@ -2,71 +2,115 @@
 #include <string>
 #include <ctime>
 using namespace std;
-//Implementare una lista concatenata semplice con le operazioni di base: inserimento in testa, in coda, eliminazione, ricerca e visualizzazione.
+
 struct Nodo {
     int dato;
-    Nodo *next;
-};typedef struct Nodo nodo;
+    Nodo* next;
+};
+
+typedef struct Nodo nodo;
 
 class Lista {
-    private:
-        Nodo *head;
-    public:
+private:
+    Nodo* head;
+public:
     Lista() {
-        head = NULL;
+        head = nullptr;
     }
+
+    // Inserisce un nodo in testa
     Nodo* testa() {
-        Nodo *p;
-        p = new Nodo;
+        Nodo* p = new Nodo;
         p->dato = rand() % 21;
         p->next = head;
         head = p;
         return p;
     }
-    void InserisciUltimo(){
-        Nodo *p;
-        p = new Nodo;
-        int NDI; //Numero da inserire
+
+    // Inserisce un nodo in coda
+    void InserisciUltimo() {
+        Nodo* p = new Nodo;
+        int NDI; // Numero da inserire
         cout << "Inserisci il numero da inserire in coda: ";
         cin >> NDI;
         p->dato = NDI;
-        p->next = NULL;
-        if(head == NULL) {
+        p->next = nullptr;
+
+        if (head == nullptr) {
             head = p;
         } else {
-            Nodo *q;
-            q = head;
-            while(q->next != NULL) {
+            Nodo* q = head;
+            while (q->next != nullptr) {
                 q = q->next;
             }
             q->next = p;
         }
     }
+
+    // Elimina il primo nodo della lista
     void Elimina() {
-        Nodo *p;
-        p = head;
-        head = head->next;
-        delete p;
+        if (head != nullptr) {
+            Nodo* p = head;
+            head = head->next;
+            delete p;
+        } else {
+            cout << "La lista è vuota. Nessun nodo da eliminare." << endl;
+        }
     }
+
+    // Visualizza tutti gli elementi della lista
     void Visualizza() {
-        Nodo *p;
-        p = head;
-        while(p != NULL) {
+        if (head == nullptr) {
+            cout << "La lista è vuota." << endl;
+            return;
+        }
+
+        Nodo* p = head;
+        while (p != nullptr) {
             cout << p->dato << " ";
             p = p->next;
         }
         cout << endl;
     }
+
+    // Ricerca un numero nella lista
+    void Ricerca() {
+        Nodo* p = head;
+        int ndr; // numero da ricercare
+        cout << "Inserisci il numero da ricercare: ";
+        cin >> ndr;
+
+        int posizione = 0;
+        while (p != nullptr) {
+            if (p->dato == ndr) {
+                cout << "Il numero è stato trovato nella posizione: " << posizione << endl;
+                return;
+            }
+            p = p->next;
+            posizione++;
+        }
+
+        cout << "Numero non trovato nella lista." << endl;
+    }
 };
 
 int main() {
-    srand(time(NULL));
+    srand(time(nullptr));
     Lista l;
-    for(int i = 0; i < 10; i++) {
+
+    for (int i = 0; i < 10; i++) {
         l.testa();
     }
-    //l.InserisciUltimo();
+
+    cout << "Lista iniziale: ";
     l.Visualizza();
+    l.Ricerca();
+    l.Elimina();
+    cout << "Lista dopo eliminazione: ";
+    l.Visualizza();
+    l.InserisciUltimo();
+    cout << "Lista dopo inserimento in coda: ";
+    l.Visualizza();
+
+    return 0;
 }
-
-
