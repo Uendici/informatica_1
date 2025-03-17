5a49c5f68d61c8eb9271eb2b9f863da4c47adcbc
Uendici <apesimone06@gmail.com>
2025-03-17 00:30:24 +0000

    waddawd

---
+++ cpp/liste_concatenate/riripasso.cpp
@@ -0,0 +1,25 @@
+//lista concatenata 
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+struct nodo{
+    int info;
+    nodo* next;
+}typedef nodo;
+
+class lista{
+    private:
+        nodo* testa;
+    public:
+        lista(){
+            testa = NULL;
+        }
+        void inserisciInTesta(){
+            nodo *p;
+            p = new nodo;
+            
+
+        }
+}
