9e08a3e32e6bee4e5baed26acd0ba1ddb364a159
Uendici <apesimone06@gmail.com>
2025-03-18 18:55:35 +0000

    riripasso

--- cpp/liste_concatenate/riripasso.cpp
+++ cpp/liste_concatenate/riripasso.cpp
@@ -1,25 +1,72 @@
-//lista concatenata 
 #include <iostream>
 #include <ctime>
 
 using namespace std;
 
-struct nodo{
+struct Nodo{
     int info;
-    nodo* next;
-}typedef nodo;
+    Nodo *next;
+};typedef struct Nodo nodo;
 
 class lista{
     private:
-        nodo* testa;
+        nodo *testa;
     public:
         lista(){
-            testa = NULL;
+            testa = nullptr;
         }
-        void inserisciInTesta(){
-            nodo *p;
-            p = new nodo;
-            
 
+        nodo *riempimento(){
+            nodo *l;
+            l = new nodo;
+            l->info = rand() % 6;
+            l->next = testa;
+            testa = l;
+            return l;
         }
+        void visualizza(){
+            nodo *l = testa;
+            while(l != nullptr){
+                cout << l->info << " ";
+                l = l->next;
+            }
+            cout << endl;
+        }
+        void ricerca(int ndc){
+            nodo *l = testa;
+            int i = 1;
+            while( l != nullptr){
+                if(ndc == l->info){
+                    cout << "il numero "<< ndc << " è stato trovato nella : " << i << " memoria" << endl;
+                }
+                l = l->next;
+                i++;
+            }
+        }
+        void elimina(int ndc){
+            nodo *l = testa;
+            nodo *precedente = nullptr;
+            int i = 1;
+            while( l != nullptr && ndc == l->info){
+            precedente = l;
+            delete l;
+            l = l->next;
+            
+        }
+    }
+};
+
+int main(){
+
+    srand(time(nullptr));
+    lista lista1;
+
+    for(int i = 0; i < 5; i++){
+        lista1.riempimento();
+    }
+    lista1.visualizza();
+    lista1.ricerca(3);
+    lista1.elimina(2);
+    cout << "con il numero eliminato" << endl;
+    lista1.visualizza();
 }
