443d5bf00e6500111cced4c56c0d8c31df2b044e
Uendici <apesimone06@gmail.com>
2025-04-02 06:32:22 +0000

    efef

--- cpp/liste_concatenate/lista.cpp
+++ cpp/liste_concatenate/lista.cpp
@@ -41,12 +41,24 @@ class lista{
             }
             k->next = l;
         }
-        void calcellazione(int ndc){//numero da cancellare 
-
+        void cancellazioneTesta(){
+            nodo *l= testa;
+            testa = testa ->next;
+            delete l;
+        }
+        int TrovaNumeriPari(){//numero da cancellare 
+            nodo *l = testa;
+            int i = 0;
+            while(l != nullptr){
+                if(l->info % 2 == 0){
+                    i++;
+                }
+                l = l->next;
+            }
+            return i;
         }
         void visualizzaLista(){
-            nodo *l = new nodo;
-            l = testa;
+            nodo *l = testa;
             while(l != nullptr){
                 cout << l->info << " ";
                 l = l->next;
@@ -57,12 +69,17 @@ class lista{
 int main(){
     lista l1;
     int info;
-
+    int numeroPari;
     for(int i = 0; i < 3; i++){
         cout << i+1 << " nodo";
         cin >> info;
         l1.inserimentoInTesta(info);
     }
-    l1.inserimentoInCoda(2);
-    l1.visualizzaLista();
+    //l1.inserimentoInCoda(3);
+    //l1.visualizzaLista();
+    //l1.cancellazioneTesta();
+    numeroPari = l1.TrovaNumeriPari();
+    cout << "i numeri pari trovati sono: " << numeroPari;
+    return 0;
 }
+
