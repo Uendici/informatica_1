d2c1a301b570dc0ede909f57861b43aafc5b8118
Uendici <apesimone06@gmail.com>
2025-04-03 08:55:55 +0000

    wdwdAWDAW

---
+++ cpp/liste_concatenate/lista_ripasso_pcto.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <random>
+#include <ctime>
+
+using namespace std;
+typedef struct Nodo{
+    Nodo *next;
+    int info;
+} nodo;
+
+class LISTA{
+    private:
+    nodo* testa;
+    public:
+    LISTA(){
+        testa = nullptr;
+    }
+    void inserimentointesta(){
+        nodo *l = new nodo;
+        l->info = rand() % 100;
+        l->next = testa;
+        testa = l;
+    }
+
+    void inserimentoincoda(){
+        nodo *l = new nodo;
+        l->info = rand() % 100;
+        l->next = nullptr;
+        if(testa == nullptr){
+            testa = l;
+        }else{
+            nodo *p = testa;
+            while(p->next != nullptr){
+                p = p->next;
+            }
+            p->next = l;
+        }
+    }
+    void stampa(){
+        nodo *p = testa;
+        while(p != nullptr){
+            cout << p->info << " ";
+            p = p->next;
+        }
+        cout << endl;
+    }
+};
+int main(){
+    srand(time(0));
+    LISTA l;
+    for(int i = 0; i < 10; i++){
+        l.inserimentointesta();
+    }
+    l.stampa();
+    for(int i = 0; i < 10; i++){
+        l.inserimentoincoda();
+    }
+    l.stampa();
+}
